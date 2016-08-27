353b5ddab9c30ae0b8f71cbbc19c9d0c3fa9c16b
Jake Dube <jake4pro@gmail.com>
2016-08-27 13:13:57 -0400

    Bumped version number to 2.4

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ from .check_for_updates import CheckForUpdates
 bl_info = {
     "name": "Modular trees",
     "author": "Herpin Maxime, Jake Dube",
-    "version": (2, 3),
+    "version": (2, 4),
     "blender": (2, 77, 0),
     "location": "View3D > Tools > Tree > Make Tree",
     "description": "Generates an organic tree with correctly modeled branching.",
