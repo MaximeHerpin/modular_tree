c32bd8692dddea4290d662847bb53c00136f8e80
Jake Dube <jake4pro@gmail.com>
2016-10-10 10:21:15 -0400

    Fixed text writing bug.

--- addon_name.py
+++ addon_name.py
@@ -1,9 +1,16 @@
+import os
+
+
+def get_file():
+    return os.path.join(os.path.dirname(__file__), "addon_name.txt")
+
+
 def get_addon_name():
-    with open("addon_name.txt", 'r') as f:
+    with open(get_file(), 'r') as f:
         name = f.read().strip()
     return name
 
 
 def save_addon_name(name):
-    with open("addon_name.txt", 'w') as f:
+    with open(get_file(), 'w') as f:
         print(name, file=f)
