51502f8dcf63820bca5d4ce5bd5c42b64315f5d1
MaximeHerpin <maxherpin44@gmail.com>
2018-02-11 15:56:04 +0100

    optimized wind as modal operator

--- wind.py
+++ wind.py
@@ -1,165 +1,146 @@
+
 import bpy
+from bpy.props import IntProperty, FloatProperty
+from mathutils import Matrix
 import numpy as np
-from mathutils import Matrix, Vector
-from math import pi, exp, atan, sqrt, cos, sin
 from collections import deque
+from mathutils import Vector, Matrix
+from math import sqrt, cos, sin, exp
 from random import random
 
 
-def wind_function(position, time, strength, direction=Vector((1, 0, 0)), random_wind_strength=0):
-    return direction * strength  # * (1-exp(-time/10)) #* (1 + .005*(random_wind_strength-.5))* (1+cos(2*pi*time/10)/80)
+class Wind1:
+    def __init__(self, armature_object):
+        self.time = 0
+        self.bones = armature_object.pose.bones
+        self.n = len(self.bones)
+        self.bone_sizes = np.zeros(self.n, dtype=np.float)
+        self.bone_lengths = np.zeros(self.n, dtype=np.float)
+        self.bone_lengths.shape = (self.n, 1)
+
+        for i, b in enumerate(self.bones):
+            self.bone_sizes[i] = b.bone.head_radius
+            self.bone_lengths[i] = b.length
+
+        inertia_moment = self.bone_lengths ** 2
+        damping = np.ones(self.n, dtype=np.float) * 10
+        damping.shape = (self.n, 1)
+        self.stiffness = self.bone_sizes * 50
+        self.stiffness[0] *= 10
+        self.stiffness.shape = (self.n, 1)
+
+        self.s = np.sqrt(np.abs(damping ** 2 - 4 * inertia_moment * self.stiffness)) / (2 * inertia_moment)
+        self.r = - damping / (2 * inertia_moment)
+
+        self.original_matrices = np.zeros(self.n * 16, dtype=np.float)
+        self.bones.foreach_get('matrix', self.original_matrices)
+
+        self.current_rotations = np.zeros(self.n * 2, dtype=np.float)
+        self.current_speeds = np.zeros(self.n * 2, dtype=np.float)
+        self.current_rotations.shape = (self.n, 2)
+        self.current_speeds.shape = (self.n, 2)
+
+    def step(self, strength=1, wind_direction=Vector((1, 0, 0))):
+        self.time += 1
+        wind_vector = np.array(wind_direction) * (1 + np.cos(self.time / 10) * random() / 4) * strength / 500
+        t = 1 / 34
+        matrices = np.zeros(self.n * 16, dtype=np.float)
+        self.bones.foreach_get('matrix', matrices)
+        matrices.shape = (self.n, 4, 4)
+        i_base = np.linalg.inv(matrices[:, :3, :3])
+        bone_vect = matrices[:, :3, 1] * self.bone_lengths
+        torque = np.einsum('ijk,ik->ij', i_base, np.cross(wind_vector, bone_vect))[:, 0:3:2]
+
+        # torque +=.01
+        torque.shape = (self.n, 2)
+        alpha = self.current_rotations
+        beta = (self.current_speeds - self.r * alpha) / self.s
+        memory = self.current_rotations.copy()
+        angle = np.exp(self.r * t) * (alpha * np.cos(self.s * t) + beta * np.sin(self.s * t)) + torque / self.stiffness
+        # angle -= memory
+        true_angle = angle - memory
+        self.current_rotations = angle
+        self.current_speeds = self.r * (angle - torque) + np.exp(self.r * t) * (
+        -alpha * self.s * np.sin(self.s * t) + beta * self.s * np.cos(self.s * t))
+
+        Cos = np.cos(true_angle)
+        Sin = np.sin(true_angle)
+        rot_X_cos = np.tile([0, 0, 0, 0, 0, 1.0, 0, 0, 0, 0, 1.0, 0, 0, 0, 0, 0], self.n)
+        rot_X_sin = np.tile([0, 0, 0, 0, 0, 0, -1.0, 0, 0, 1.0, 0, 0, 0, 0, 0, 0], self.n)
+        rot_X_cos.shape = (self.n, 16)
+        rot_X_cos *= Cos[:, 0].reshape(self.n, 1)
+        rot_X_sin.shape = (self.n, 16)
+        rot_X_sin *= Sin[:, 0].reshape(self.n, 1)
+        diag = np.tile([1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0], self.n)
+        diag.shape = (self.n, 16)
+        rot_X = rot_X_cos + rot_X_sin + diag
+        rot_X.shape = (self.n, 4, 4)
+
+        rot_Z_cos = np.tile([1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, 0, 0, 0, 0, 0], self.n)
+        rot_Z_sin = np.tile([0, 0, 1.0, 0, 0, 0, 0, 0, -1.0, 0, 0, 0, 0, 0, 0, 0], self.n)
+        rot_Z_cos.shape = (self.n, 16)
+        rot_Z_cos *= Cos[:, 1].reshape(self.n, 1)
+        rot_Z_sin.shape = (self.n, 16)
+        rot_Z_sin *= Sin[:, 1].reshape(self.n, 1)
+        diag = np.tile([0, 0, 0, 0, 0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0], self.n)
+        diag.shape = (self.n, 16)
+        rot_Z = rot_Z_cos + rot_Z_sin + diag
+        rot_Z.shape = (self.n, 4, 4)
+
+        # for i in range(self.n):
+        #    matrices[i] = matrices[i].dot(rot_X[i])
+        # matrices[i] = matrices[i].dot(np.array([list(i.to_tuple()) for i in list(Matrix.Rotation(true_angle[i,0], 4, 'X'))]))
+        # matrices[i] = matrices[i].dot(np.array([list(i.to_tuple()) for i in list(Matrix.Rotation(angle[i,1] - memory[i,1], 4, 'Y'))]))
+        # self.bones.foreach_set('matrix', matrices.ravel())
+        self.bones.foreach_set('matrix',
+                               np.einsum('hik,hkj->hij', np.einsum('hik,hkj->hij', matrices, rot_Z), rot_X).ravel())
+        # bones.foreach_set('matrix', matrices.ravel())
+        # bpy.ops.object.mode_set(mode='EDIT')
+        # bpy.ops.object.mode_set(mode='OBJECT')
+        bpy.context.scene.frame_set(1)
 
 
-def wind(armature_object):
-    print("-" * 50)
-    bones = organize_bones(armature_object)
-    n = len(bones)
-    # for b in bones:
-    #     b.matrix *= Matrix.Rotation(pi/20, 4, 'X')
-    #
-    #     armature_object.pose.update()
+class ModalOperator(bpy.types.Operator):
+    """Move an object with the mouse, example"""
+    bl_idname = "object.modal_operator"
+    bl_label = "Simple Modal Operator"
 
 
-    original_directions = np.ones(3 * n, dtype=np.float)
+    _timer = None
+    wind = None
+    wind_object = None
 
-    rotation_speed = np.zeros(2 * n, dtype=np.float)
-    rotation_speed.shape = (n, 2)
-    original_matrices = [b.matrix for b in bones]
-    current_rotation = np.zeros(2 * n, dtype=np.float)
-    current_rotation.shape = (n, 2)
-    original_directions.shape = (n, 3)
-    for i in range(len(bones)):
-        original_directions[i] = [k for k in bones[i].y_axis.normalized().to_tuple()]
+    def modal(self, context, event):
+        if event.type in {'ESC'}:
+            self.cancel(context)
+            return {'CANCELLED'}
 
-    for b in bones:
-        b.rotation_mode = 'QUATERNION'
-        b.keyframe_insert(data_path='rotation_quaternion', frame=0)
+        if event.type == 'TIMER':
+            # delta = self.first_mouse_x - event.mouse_x
+            # context.object.location.x = self.first_value + delta * 0.01
+            # numpy_wind(context.object)
 
-    wind_step(bones, original_matrices, current_rotation, rotation_speed)
+            self.wind.step(self.wind_object.field.strength,
+                           Matrix.Rotation(self.wind_object.rotation_euler.z, 3, 'Z') * Vector((1, 0, 0)))
 
-    bpy.ops.object.mode_set(mode='EDIT')
-    bpy.ops.object.mode_set(mode='OBJECT')
+        if event.type == 'NUMPAD_PLUS':
+            self.strength += .1
+        if event.type == 'NUMPAD_MINUS':
+            self.strength -= .1
 
+        return {'PASS_THROUGH'}
 
-def wind_step(bones, original_matrices, current_rotation, rotation_speed):
-    for frame in range(100):
-        random_wind_strength = random()
-        for i, b in enumerate(bones):
+    def execute(self, context):
+        wm = context.window_manager
+        self._timer = wm.event_timer_add(0.1, context.window)
+        wm.modal_handler_add(self)
+        self.wind = Wind1(context.object)
+        self.wind_object = context.scene.objects['Field']
+        return {'RUNNING_MODAL'}
 
-            wind_vector = wind_function(b.tail, frame, .5, Vector((1, 0, 0)))
-
-            base = b.matrix.to_3x3()
-            i_base = base.inverted()
-            bone_vector = (b.tail - b.head)
-
-            inertia_moment = b.length ** 2
-            damping = 5
-            stiffness = (b.bone.head_radius) * 500
-
-            torque = i_base * wind_vector.cross(bone_vector)  # / 7000 /(b.bone.head_radius)**2
-            if b.parent is not None and len(b.parent.children) > 1:
-                stiffness *= 2
-                torque /= 3
-            torque_x = torque.x
-            torque_z = torque.z
-            timestep = 1 / 175
-            r0x, r1x = general_solution_second_order(inertia_moment, damping, stiffness, torque_x, current_rotation[i][0], rotation_speed[i][0], timestep)
-            r0z, r1z = general_solution_second_order(inertia_moment, damping, stiffness, torque_z, current_rotation[i][0], rotation_speed[i][0], timestep)
-            rx, rz = current_rotation[i]
-            current_rotation[i] = (r0x, r0z)
-            rotation_speed[i] = (r1x, r1z)
-
-            # direction = compute_new_direction(Vector(original_directions[i]), bone_vector, wind_vector, Vector(bones_inertia[i]), effective_surface, stiffness, frame)
-            # direction = wind_vector
-            # angle = cos(frame/10 + random_phase[i]) * effective_surface/100
-            # bones_inertia[i] = np.array(list(direction.to_tuple()))
-            #
-            # up = b.y_axis
-            #
-            # # print(base)
-            #
-            # axis = base.inverted() * up.cross(direction)
-            # rot = Matrix.Rotation(angle, 4, axis)
-            # b.matrix *= rot
-            # b.rotation_mode = 'QUATERNION'
-
-            b.matrix = original_matrices[i] * Matrix.Rotation(r0x - rx, 4, 'X') * Matrix.Rotation(r0z - rz, 4, 'Z')
-            b.keyframe_insert(data_path='rotation_quaternion', frame=frame + 1)
-            # bpy.context.scene.update()
-        bpy.context.scene.update()
-
-
-def wind_solver(bones, original_matrices, current_rotation, rotation_speed):
-    for frame in range(100):
-        random_wind_strength = random()
-        for i, b in enumerate(bones):
-
-            mass = (b.bone.head_radius) ** 2 * b.length
-            barycenter = b.tail * mass
-            for c in b.children:
-                mass += c["mass"]
-                barycenter += Vector(c["barycenter"])
-
-            b["mass"] = mass
-            b["barycenter"] = barycenter
-            barycenter /= mass
-
-            wind_vector = wind_function(b.tail, frame, .3, Vector((1, 0, 0)), random_wind_strength)
-
-            base = b.matrix.to_3x3()
-            i_base = base.inverted()
-            bone_vector = barycenter - b.head
-
-            inertia_moment = bone_vector.length ** 2 * mass / 10000
-            damping = 0.5 * b.bone.head_radius
-            stiffness = (b.bone.head_radius) ** 2 / b.length * 800
-            torque = i_base * wind_vector.cross(bone_vector) / 7000 / (b.bone.head_radius) ** 2
-            torque_x = torque.x
-            print(torque_x)
-            torque_z = torque.z
-            timestep = 1 / 24
-            r0x, r1x = general_solution_second_order(inertia_moment, damping, stiffness, torque_x, current_rotation[i][0], rotation_speed[i][0], timestep)
-            r0z, r1z = general_solution_second_order(inertia_moment, damping, stiffness, torque_z, current_rotation[i][0], rotation_speed[i][0], timestep)
-            # r0x = .001/b.bone.head_radius  + cos(2*pi*frame/200 * mass + mass)/(100 * b.bone.head_radius)
-            rx, rz = current_rotation[i]
-            current_rotation[i] = (r0x, r0z)
-            rotation_speed[i] = (r1x, r1z)
-
-            # direction = compute_new_direction(Vector(original_directions[i]), bone_vector, wind_vector, Vector(bones_inertia[i]), effective_surface, stiffness, frame)
-            # direction = wind_vector
-            # angle = cos(frame/10 + random_phase[i]) * effective_surface/100
-            # bones_inertia[i] = np.array(list(direction.to_tuple()))
-            #
-            # up = b.y_axis
-            #
-            # # print(base)
-            #
-            # axis = base.inverted() * up.cross(direction)
-            # rot = Matrix.Rotation(angle, 4, axis)
-            # b.matrix *= rot
-            # b.rotation_mode = 'QUATERNION'
-
-            b.matrix = original_matrices[i] * Matrix.Rotation(r0x - rx, 4, 'X') * Matrix.Rotation(r0z - rz, 4, 'Z')
-            b.keyframe_insert(data_path='rotation_quaternion', frame=frame + 1)
-            # bpy.context.scene.update()
-        bpy.context.scene.update()
-
-
-def compute_new_direction(original_direction, current_direction, wind_vector, bone_inertia, effective_surface, stiffness):
-    new_direction = current_direction + bone_inertia * 0 + wind_vector * effective_surface + (original_direction - current_direction) * stiffness
-    new_direction = current_direction + wind_vector + (original_direction - current_direction) * stiffness
-    new_direction = current_direction + wind_vector / effective_surface
-    return new_direction
-
-
-def getRoll(bone):
-    mat = bone.matrix_local.to_3x3()
-    quat = mat.to_quaternion()
-    if abs(quat.w) < 1e-4:
-        roll = pi
-    else:
-        roll = 2 * atan(quat.y / quat.w)
-    return roll
+    def cancel(self, context):
+        wm = context.window_manager
+        wm.event_timer_remove(self._timer)
 
 
 def organize_bones(armature_object):
@@ -188,7 +169,6 @@ def general_solution_second_order(a, b, c, d, x0, x1, t):
 
     delta = b ** 2 - 4 * a * c
     if delta > 0:
-        print('d>0')
         r_delta = sqrt(delta)
         r1 = (-b - r_delta) / (2 * a)
         r2 = (-b + r_delta) / (2 * a)
@@ -199,8 +179,7 @@ def general_solution_second_order(a, b, c, d, x0, x1, t):
         return alpha * exp(r1 * t) + beta * exp(r2 * t) + constant_sol, derived
 
     else:
-        print('cos')
-        s = sqrt(-delta)
+        s = sqrt(-delta) / (2 * a)
         r = -b / (2 * a)
         alpha = x0
         beta = (x1 - r * alpha) / s
@@ -210,62 +189,16 @@ def general_solution_second_order(a, b, c, d, x0, x1, t):
         return sol, derived
 
 
-def add_f_curve_modifiers(armature_object):
-    strength = .1
-    wind_vector = Vector((1, 0, 0)) * strength
-
-    bones = organize_bones(armature_object)
-
-    for b in bones:
-        mass = (b.bone.head_radius) ** 2 * b.length
-        barycenter = b.tail * mass
-        for c in b.children:
-            mass += c["mass"]
-            barycenter += Vector(c["barycenter"])
-        b["mass"] = mass
-        b["barycenter"] = barycenter
-        barycenter /= mass
-
-        b.rotation_mode = 'XYZ'
-        b.keyframe_insert('rotation_euler', frame=0, index=0)
-        b.keyframe_insert('rotation_euler', frame=0, index=2)
-
-    fcurves = armature_object.animation_data.action.fcurves
-
-    for i in range(len(bones)):
-        f0 = fcurves[2 * i]
-        f1 = fcurves[2 * i + 1]
-        b = bones[i]
-
-        i_base = b.matrix.to_3x3().inverted()
-        bone_vector = b.tail - b.head
-
-        inertia_moment = bone_vector.length ** 2 * mass / 10000
-        damping = 0.5 * b.bone.head_radius
-        stiffness = (b.bone.head_radius) ** 2 / b.length * 800
-        if b.parent is not None and len(b.parent.children) > 1:
-            stiffness *= 2
-            torque /= 3
-        else:
-            torque = Vector((0, 0, 0))
-        torque = i_base * wind_vector.cross(bone_vector) / b.bone.head_radius / 1000
-
-        f = sqrt(abs(damping ** 2 - 4 * inertia_moment * stiffness)) / 10
-
-        x_amplitude = torque.x
-        z_amplitude = torque.z
-
-        m0 = f0.modifiers.new(type='FNGENERATOR')
-        m1 = f1.modifiers.new(type='FNGENERATOR')
-        m0.function_type = 'SIN'
-        m1.function_type = 'SIN'
-        m0.amplitude = x_amplitude
-        m1.amplitude = z_amplitude
-        m0.phase_multiplier = f
-        m1.phase_multiplier = f
-        m0.value_offset = x_amplitude * 3
-        m1.value_offset = z_amplitude * 3
+def register():
+    bpy.utils.register_class(ModalOperator)
 
 
-# wind(bpy.context.object)
-add_f_curve_modifiers(bpy.context.object)
+def unregister():
+    bpy.utils.unregister_class(ModalOperator)
+
+
+if __name__ == "__main__":
+    register()
+
+    # test call
+    bpy.ops.object.modal_operator('INVOKE_DEFAULT')
