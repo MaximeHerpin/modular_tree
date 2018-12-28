55abdda8905ba0c4beae74d4d4d1746b13776978
MaximeHerpin <maxherpin44@gmail.com>
2018-12-28 14:24:57 +0100

    added position in branch property

--- tree_node.py
+++ tree_node.py
@@ -12,6 +12,7 @@ class MTreeNode:
         self.growth_goal = 0 # float - How much the node should be grown.
         self.growth_radius = 0 # float - The radius of the node when it first started growing
         self.can_be_splitted = True # bool - if true the node can be splitted (can have more than 1 children)
+        self.position_in_branch = 0 # float - 0 when node is at the begining of a branch, 1 when it is at the end
         self.is_branch_origin = False
 
     def get_grow_candidates(self, candidates, creator):
@@ -34,6 +35,27 @@ class MTreeNode:
         
         return min_height, max_height
 
+
+    def set_positions_in_branches(self, current_distance=0, distance_from_parent=0):
+        ''' set each node position_in_branch property to it's correct value
+            to do so, the function returns the length of current branch '''
+
+        current_distance += distance_from_parent # increase current distance by the distance from node parent
+
+        if len(self.children) == 0: # if node is the end of the branch, return the length of the branch and set the position_in_branch to 1
+            self.position_in_branch = 1
+            return current_distance
+        
+        for child in self.children[1:]: # recursivelly call the function on all side children
+            child.set_positions_in_branches(0, 0) # the current_distance of a side child is 0 since it is the begining of a new branch
+
+        distance_to_child = (self.position - self.children[0].position).length
+        branch_length = self.children[0].set_positions_in_branches(current_distance, distance_to_child)
+
+        self.position_in_branch = 0 if branch_length==0 else current_distance / branch_length
+        print(self.position_in_branch)
+        return branch_length
+        
     def get_split_candidates(self, candidates, creator, offset, current_offset=0):
         if len(self.children) == 1 and not self.is_branch_origin and self.creator == creator and current_offset >= offset:
             candidates.append(self)
