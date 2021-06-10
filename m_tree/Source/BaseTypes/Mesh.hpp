428d7268bc5d897f0cf1137fd27ee4fabc5b9e04
MaximeHerpin <maxime@prevu3d.com>
2021-06-10 18:57:46 -0400

    better structure

--- m_tree/Source/BaseTypes/Mesh.hpp
+++ m_tree/Source/BaseTypes/Mesh.hpp
@@ -3,15 +3,15 @@
 #include <array>
 #include <Eigen/Core>
 
-#include<pybind11/pybind11.h>
-#include<pybind11/numpy.h>
+// #include<pybind11/pybind11.h>
+// #include<pybind11/numpy.h>
 
 namespace Mtree
 {
 	using Vector3 = Eigen::Vector3f;
 	using Vector2 = Eigen::Vector2f;
 	
-	namespace py = pybind11;
+	// namespace py = pybind11;
 
 	class Mesh
 	{
@@ -28,38 +28,38 @@ namespace Mtree
 		std::vector<std::vector<int>> get_polygons() { return this->polygons; };
 
 		
-		py::array_t<float> get_vertices_numpy()
-		{
-			auto result = py::array_t<float>(vertices.size() * 3);
-			py::buffer_info buff = result.request();
+		// py::array_t<float> get_vertices_numpy()
+		// {
+		// 	auto result = py::array_t<float>(vertices.size() * 3);
+		// 	py::buffer_info buff = result.request();
 
-			float* ptr = (float*)buff.ptr;
-			for (int i = 0; i < vertices.size(); i++)
-			{
-				for (size_t j = 0; j < 3; j++)
-				{
-					ptr[i*3+j] = vertices[i][j];
-				}
-			}
+		// 	float* ptr = (float*)buff.ptr;
+		// 	for (int i = 0; i < vertices.size(); i++)
+		// 	{
+		// 		for (size_t j = 0; j < 3; j++)
+		// 		{
+		// 			ptr[i*3+j] = vertices[i][j];
+		// 		}
+		// 	}
 
-			return result;
-		};
+		// 	return result;
+		// };
 
-		py::array_t<int> get_polygons_numpy()
-		{
-			auto result = py::array_t<int>(polygons.size() * 4);
-			py::buffer_info buff = result.request();
+		// py::array_t<int> get_polygons_numpy()
+		// {
+		// 	auto result = py::array_t<int>(polygons.size() * 4);
+		// 	py::buffer_info buff = result.request();
 
-			int* ptr = (int*)buff.ptr;
-			for (int i = 0; i < polygons.size(); i++)
-			{
-				for (int j = 0; j < 4; j++)
-				{
-					ptr[i * 4 + j] = polygons[i][j];
-				}
-			}
-			return result;
-		};
+		// 	int* ptr = (int*)buff.ptr;
+		// 	for (int i = 0; i < polygons.size(); i++)
+		// 	{
+		// 		for (int j = 0; j < 4; j++)
+		// 		{
+		// 			ptr[i * 4 + j] = polygons[i][j];
+		// 		}
+		// 	}
+		// 	return result;
+		// };
 
 	};
 }
