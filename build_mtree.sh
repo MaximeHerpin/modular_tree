4ba4ec89189700ae4ed630956320670dedd123d0
MaximeHerpin <maxime@prevu3d.com>
2021-07-10 17:43:56 -0400

    fix python library linux name

--- build_mtree.sh
+++ build_mtree.sh
@@ -1,5 +1,5 @@
 python3 m_tree/install.py
-cp ./m_tree/binaries/m_tree.cpython-37-x86_64-linux-gnu.so ./m_tree.cpython-37-x86_64-linux-gnu.so
+cp ./m_tree/binaries/m_tree.cpython-37m-x86_64-linux-gnu.so ./m_tree.cpython-37m-x86_64-linux-gnu.so
 
 
 
