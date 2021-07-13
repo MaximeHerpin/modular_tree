fc53055eff80ee6cc807a6a98df503832f093a37
MaximeHerpin <maxime@prevu3d.com>
2021-07-13 15:40:02 -0400

    correct library filenames

--- build_mtree.bat
+++ build_mtree.bat
@@ -1,6 +1,6 @@
 python m_tree/install.py
 
-COPY ".\m_tree\binaries\Release\m_tree.cp37-win_amd64.pyd" "./m_tree.cp37-win_amd64.pyd"
+COPY ".\m_tree\binaries\Release\m_tree.cp39-win_amd64.pyd" "./m_tree.cp39-win_amd64.pyd"
 
 PAUSE
 
