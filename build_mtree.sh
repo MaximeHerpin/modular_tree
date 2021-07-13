8778ca8577c79f95a6caadcd1edad604a895d3ec
MaximeHerpin <maxime@prevu3d.com>
2021-07-13 15:43:47 -0400

    correct library filename

--- build_mtree.sh
+++ build_mtree.sh
@@ -1,5 +1,5 @@
 python3 m_tree/install.py
-cp ./m_tree/binaries/m_tree.cpython-39m-x86_64-linux-gnu.so ./m_tree.cpython-39m-x86_64-linux-gnu.so
+cp ./m_tree/binaries/m_tree.cpython-39-x86_64-linux-gnu.so ./m_tree.cpython-39-x86_64-linux-gnu.so
 
 
 
