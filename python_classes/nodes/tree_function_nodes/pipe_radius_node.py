cd7c77307642b5695b52ec5f0329becfc1327358
MaximeHerpin <maxime@prevu3d.com>
2021-09-06 20:43:00 -0400

    better pire radius node defaults

--- python_classes/nodes/tree_function_nodes/pipe_radius_node.py
+++ python_classes/nodes/tree_function_nodes/pipe_radius_node.py
@@ -12,7 +12,7 @@ class PipeRadiusNode(bpy.types.Node, MtreeFunctionNode):
     def init(self, context):
         self.add_input("mt_TreeSocket", "Tree", is_property=False)
 
-        self.add_input("mt_FloatSocket", "End Radius", min_value=0.0001, property_name="end_radius", property_value=.01)
+        self.add_input("mt_FloatSocket", "End Radius", min_value=0.0001, property_name="end_radius", property_value=.005)
         self.add_input("mt_FloatSocket", "Constant Growth", min_value=0, property_name="constant_growth", property_value=.2)
         self.add_input("mt_FloatSocket", "Accumulation Power", min_value=0.1, property_name="power", property_value=2.5)
 
