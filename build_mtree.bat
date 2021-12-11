13d249c89c1580140178d5d20e1584c5d7bdfd82
MaximeHerpin <maxime@prevu3d.com>
2021-12-10 21:26:28 -0500

    output bould library in the right directory

--- build_mtree.bat
+++ build_mtree.bat
@@ -1,7 +1,3 @@
 py -3.9 m_tree/install.py
 
-COPY ".\m_tree\binaries\Release\m_tree.cp39-win_amd64.pyd" "./m_tree.cp39-win_amd64.pyd"
-
-PAUSE
-
 
--- build_mtree.sh
+++ build_mtree.sh
@@ -1,5 +1,4 @@
 python3 m_tree/install.py
-cp ./m_tree/binaries/m_tree.cpython-39-x86_64-linux-gnu.so ./m_tree.cpython-39-x86_64-linux-gnu.so
 
 
 
